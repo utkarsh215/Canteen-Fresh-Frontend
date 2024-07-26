1e329bf76a7592d143ceba6deaba92b9d534640c
utkarsh215 <115106879+utkarsh215@users.noreply.github.com>
2024-07-27 02:51:10 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(){
     return(<>
 <footer class="bg-yellow-400 shadow">
     <div class="p-2  w-full mx-auto max-w-screen-xl flex justify-center">
-      <span class="text-sm text-black-500 sm:text-center">© 2024 <a href="https://flowbite.com/" class="hover:underline">Canteen Fresh™</a>. All Rights Reserved.
+      <span class="text-sm text-black-500 sm:text-center">© 2024 <p class="hover:underline">Canteen Fresh™</p>. All Rights Reserved.
     </span>
     </div>
 </footer>
